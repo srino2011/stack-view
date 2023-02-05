5a8feda6596e072f7b91185fba4f851cd26c7402
Rino Samuel <rinosamu@gmail.com>
2023-02-05 10:50:27 +0530

    added ability to add buttons to stackview at runtime

--- StackView/ViewController.swift
+++ StackView/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var StackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onAddClicked(_ sender: UIButton) {
+        
+        let button = UIButton(frame: sender.frame)
+        button.backgroundColor = UIColor.purple
+        button.setTitle("Text", for: .normal)
+        
+        StackView.addArrangedSubview(button)
+        
+    }
+    
 }
 
